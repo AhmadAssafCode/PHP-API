7e596561fee246d7c120debe60b8a8f08d67597f
AhmadAssafCode <93490733+AhmadAssafCode@users.noreply.github.com>
2025-05-07 02:25:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 $items = [
     ["id" => 1, "name" => "Sami Ali", "email" => "sami22@gmail.com", "age" => 20, "image"=> "https://i.pravatar.cc/151"],
-    ["id" => 2, "name" => "Mazen","email" => "mazento20@gmail.com", "age" => 15, "image"=> "https://i.pravatar.cc/152"],
+    ["id" => 2, "name" => "Mazen","email" => "mazento202@gmail.com", "age" => 15, "image"=> "https://i.pravatar.cc/152"],
     ["id" => 3, "name" => "Ahmad","email" => "ahmadto20@gmail.com", "age" => 35, "image"=> "https://i.pravatar.cc/153"],
     ["id" => 4, "name" => "Mohammd","email" => "mohammadto20@gmail.com", "age" => 21, "image"=> "https://i.pravatar.cc/154"],
     ["id" => 5, "name" => "Alaa","email" => "alaato20@gmail.com","age" => 22, "image"=> "https://i.pravatar.cc/155"],
